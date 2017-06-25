29db80b14d7820a7eb36320982b09383fdb6770a
jc2729 <jc2729@cornell.edu>
2017-06-24 23:46:18 -0700

    Restructured code to display foes and friends as different sections

--- Project3/Project3/FeedViewController.swift
+++ Project3/Project3/FeedViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class Friend {
+
+class Individual {
     var name: String
     var age: Int
     var interests: [String] = [String]()
@@ -20,6 +21,12 @@ class Friend {
         self.image = image
     }
     
+}
+
+class Friend: Individual {
+    
+}
+class Foe: Individual {
     
 }
 
@@ -27,7 +34,9 @@ class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     var tableView: UITableView!
     var friends: [Friend] = [Friend]()
-    var lastSelectedIndex: Int?
+    var foes: [Foe] = [Foe]()
+
+    var lastSelectedIndex: IndexPath?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,11 +44,13 @@ class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDe
         view.backgroundColor = .white
         
         listFriends()
+        listFoes()
         
         tableView = UITableView(frame: view.frame)
         tableView.dataSource = self
         tableView.delegate = self
         tableView.tableFooterView = UIView()
+        
         
         let addFriendBarButton = UIBarButtonItem(title: "Add Friend", style: UIBarButtonItemStyle.plain, target: self, action: #selector(addFriend))
         self.navigationItem.setRightBarButton(addFriendBarButton, animated: true)
@@ -70,19 +81,59 @@ class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDe
         let friend4 = Friend(name: "Eric", age: 27, interests: ["uniqlo", "shopping"])
         let friend5 = Friend(name: "Rachel", age: 28, interests: ["memeing","napping"])
         let friend6 = Friend(name: "Olivia", age: 19, interests: ["math"])
-        
         friends = [friend1, friend2, friend3, friend4, friend5, friend6]
+    }
+    
+    func listFoes() {
+        
+        let foe1 = Foe(name: "Jealousy", age: 100, interests: ["cynicism", "skepticism"])
+        let foe2 = Foe(name: "Egotism", age: 101, interests: ["rudeness"])
+        let foe3 = Foe(name: "Greed", age: 110, interests: ["avarice"])
+        foes = [foe1, foe2, foe3]
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var rowCount = 0
+        if section == 0 {
+            rowCount = friends.count
+        }
+        else if section == 1 {
+            rowCount = foes.count
+        }
+        return rowCount
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        var title = ""
+        if section == 0 {
+            title = "Friends"
+        }
+        else if section == 1 {
+            title = "Foes"
+        }
+        return title
     }
     
     //add picture, age, name to cell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let section = indexPath.section
         
         let cell = UITableViewCell(style: .default, reuseIdentifier: "")
+        if section == 0 {
+            let friend = friends[indexPath.row]
+            cell.textLabel?.text = friend.name + " is " + String(friend.age) + " years old "
+            cell.imageView?.image = friend.image
+        }
+        else if section == 1 {
+            let foe = foes[indexPath.row]
+            cell.textLabel?.text = foe.name + " is " + String(foe.age) + " years old "
+            cell.imageView?.image = foe.image
+        }
         
-        let friend = friends[indexPath.row]
-        
-        cell.textLabel?.text = friend.name + " is " + String(friend.age) + " years old "
-        cell.imageView?.image = friend.image
         let singleTap = UITapGestureRecognizer(target: self, action: #selector(addProfilePicture))
         singleTap.numberOfTapsRequired = 1
         cell.imageView?.isUserInteractionEnabled = true
@@ -93,21 +144,20 @@ class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDe
     }
     
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return friends.count
-    }
-    
-    //pushes a controller containing a list of the friend's interests
+    //pushes a controller containing a list of the friend/foe's interests
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 
         tableView.deselectRow(at: indexPath, animated: true)
-        let friend = friends[indexPath.row]
         
-        let detailViewController = DetailViewController()
-        detailViewController.interests = friend.interests
-        detailViewController.delegate = self
-        navigationController?.pushViewController(detailViewController, animated: true)
-        
+        var individual: Individual = Individual(name: "", age: 0, interests: [""])
+        let section = indexPath.section
+        if section == 0 {
+            individual = friends[indexPath.row]
+            let detailViewController = DetailViewController()
+            detailViewController.interests = individual.interests
+            detailViewController.delegate = self
+            navigationController?.pushViewController(detailViewController, animated: true)
+        }
     }
     
     //challenge 1: set different heights for the rows
@@ -120,8 +170,17 @@ class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     //challenge 5: allow removing friends
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        let section = indexPath.section
+        
         if (editingStyle == UITableViewCellEditingStyle.delete) {
-            friends.remove(at: indexPath.row)
+            
+            if section == 0 {
+                friends.remove(at: indexPath.row)
+            }
+            else if section == 1 {
+                foes.remove(at: indexPath.row)
+            }
         }
         tableView.reloadData()
     }
@@ -130,7 +189,7 @@ class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDe
         return true
     }
     
-    //challenge 2: allow editing interests
+    //challenge 2: allow editing friend's interests
     func updateInterests(oldInterests: [String], updatedInterests: [String]) {
       
         for friend in friends{
@@ -144,7 +203,7 @@ class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDe
     //challenge 3: allow adding pictures from camera roll, source from either camera roll or photo library
     func addProfilePicture(sender: UITapGestureRecognizer) {
         let tapLocation = sender.location(in: tableView)
-        lastSelectedIndex = tableView.indexPathForRow(at: tapLocation)?.row
+        lastSelectedIndex = tableView.indexPathForRow(at: tapLocation)
         let imagePickerController = UIImagePickerController()
         imagePickerController.delegate = self
         
@@ -153,15 +212,19 @@ class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDe
         } else {
             imagePickerController.sourceType = .photoLibrary
         }
-        
+        print("adding profile pic")
         present(imagePickerController, animated: true, completion: nil)
     }
     
     //select picture to add
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        
         if let photo = info[UIImagePickerControllerOriginalImage] as? UIImage{
-            friends[lastSelectedIndex!].image = photo
+            if lastSelectedIndex!.section == 0 {
+                friends[lastSelectedIndex!.row].image = photo
+            }
+            else if lastSelectedIndex!.section == 1 {
+                foes[lastSelectedIndex!.row].image = photo
+            }
         }
         
         picker.dismiss(animated: true, completion: nil)
